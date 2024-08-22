d3d8fc5be0a63391e107e93b4d89982897918624
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-08-22 19:52:46 +0000

    add login/logout and token updates to auth service

--- frontend/shared/auth.service.ts
+++ frontend/shared/auth.service.ts
@@ -2,10 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LOGIN, SIGN_UP } from './constants/urls';
 import { AuthModel } from './models/auth.model';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  private token!: string;
+  private authenticatedSub = new Subject<boolean>();
+  private isAuthenticated = false;
+
+  getToken() {
+    return this.token;
+  }
+
+  getAuthenticatedSub() {
+    return this.authenticatedSub.asObservable();
+  }
+
+  getIsAuthenticated() {
+    return this.isAuthenticated;
+  }
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   signUpUser(username: string, password: string) {
     const data: AuthModel = { username: username, password: password };
@@ -18,8 +36,31 @@ export class AuthService {
   loginUser(username: string, password: string) {
     const data: AuthModel = { username: username, password: password };
 
-    this.http.post(LOGIN, data).subscribe((res) => {
+    this.http.post<{ token: string }>(LOGIN, data).subscribe((res) => {
       console.log(res);
+      this.token = res.token;
+      if (this.token) {
+        this.authenticatedSub.next(true);
+        this.isAuthenticated = true;
+        this.storeToken(this.token);
+        this.router.navigate(['/']);
+      }
     });
+  }
+
+  logOutUser() {
+    this.token = '';
+    this.authenticatedSub.next(false);
+    this.isAuthenticated = false;
+    this.removeToken();
+    this.router.navigate(['/login']);
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
   }
 }
