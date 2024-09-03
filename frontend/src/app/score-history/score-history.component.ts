acf01174f2a4faa4a428cc38246c217795c658e1
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-09-03 21:52:54 +0000

    create score history component

---
+++ frontend/src/app/score-history/score-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './score-history.component.html',
+  styleUrl: './score-history.component.css'
+})
+export class ScoreHistoryComponent {
+
+}
