3a27712b4745d189d2f5c3978ebc63fae1169f53
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-09-12 15:06:32 +0000

    add fields to user model

--- frontend/shared/models/user.model.ts
+++ frontend/shared/models/user.model.ts
@@ -1,5 +1,9 @@
+import { Boardgame } from './boardgame.model';
+
 export interface User {
   username: string;
   photo_url: string;
   _id: string;
+  friends?: string[];
+  boardgames?: Boardgame[];
 }
