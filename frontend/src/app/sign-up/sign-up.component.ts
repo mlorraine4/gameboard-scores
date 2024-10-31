0d5325a77111b07a8acbb9a9e4e40f2505b73fab
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-10-31 22:21:13 +0000

    add error handling service to sign up

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -7,20 +7,29 @@ import {
 } from '@angular/forms';
 import { AuthService } from '../../../shared/services/auth.service';
 import { NgIf } from '@angular/common';
-import { passwordCheckValidator } from '../../../shared/password-check.directive';
+import { passwordCheckValidator } from '../../../shared/directives/password-check.directive';
+import { ErrorHandlingService } from '../../../shared/services/errorHandling.service';
+import { SuccessHandlingService } from '../../../shared/services/successHandling.service';
+import { SuccessComponent } from '../shared/success/success.component';
+import { ErrorComponent } from '../shared/error/error.component';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, SuccessComponent, ErrorComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
   signUpForm!: FormGroup;
-
-  constructor(private authService: AuthService) {}
-
+  success: Boolean = false;
+  constructor(
+    private authService: AuthService,
+    private errorHandlingService: ErrorHandlingService,
+    private successHandlingService: SuccessHandlingService
+  ) {}
+  // TODO: check if username is not taken after touched/dirty and valid
   ngOnInit(): void {
     this.signUpForm = new FormGroup(
       {
@@ -43,19 +52,31 @@ export class SignUpComponent implements OnInit {
       { validators: passwordCheckValidator }
     );
   }
-
   get username() {
     return this.signUpForm.get('username');
   }
-
   get password() {
     return this.signUpForm.get('password');
   }
 
   onSubmit() {
-    this.authService.signUpUser(
-      this.signUpForm.value.username,
-      this.signUpForm.value.password
-    );
+    this.authService
+      .signUpUser(
+        this.signUpForm.value.username,
+        this.signUpForm.value.password
+      )
+      .pipe(
+        catchError(
+          // TODO: check the status of err?
+          this.errorHandlingService.handleError('Username is already taken.')
+        )
+      )
+      .subscribe((res) => {
+        if (res.ok) {
+          this.successHandlingService.handleSuccess('Sign up successful!');
+          this.success = true;
+          this.signUpForm.reset();
+        }
+      });
   }
 }
